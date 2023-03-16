c579783dd1b80fdb7b5a8747f239f3d1ae756551
matthew-io <xyzmonte@protonmail.com>
2023-03-16 13:00:44 +0000

    Initial commit

---
+++ src/com/company/Profile.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Profile {
+    private String username;
+    private String firstName;
+    private String surname;
+    private Profile[] friends;
+    private String workplace;
+    private String hometown;
+    private int userID;
+
+    public Profile() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Profile[] getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Profile[] friends) {
+        this.friends = friends;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+}
