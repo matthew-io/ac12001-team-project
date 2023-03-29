be4f0805b63d778da16e91d28ffc5bb0f1df8687
Pronamel <114414390+Pronamel@users.noreply.github.com>
2023-03-29 15:40:11 +0100

    adding the home page methods and instance var to network

--- src/com/company/Network.java
+++ src/com/company/Network.java
@@ -12,10 +12,14 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Network {
     // instance variables
     Profile root;
+    LinkedList<post> list = new LinkedList<>();
     int totalProfiles;
 
     /**
@@ -24,6 +28,152 @@ public class Network {
     Network() {
         totalProfiles = 0;
     }
+
+
+
+    //posts methods
+    public void PagePosts(){
+        String navigation;
+        int endValue = list.size();
+        int currentValue = 0;
+
+        System.out.println(list.get(currentValue).getUserName());
+        while(true){
+
+            while(true){
+                System.out.println("L to go left R to go right X to quit");
+                Scanner myObj = new Scanner(System.in);
+                String text = myObj.nextLine();
+
+                if(text.equals("L") || text.equals("R") || text.equals("X")){navigation = text; break;}
+            }
+
+            if(navigation.equals("L")){
+                if(currentValue == 0){currentValue = endValue - 1;}
+                else{currentValue --;}
+            }
+            else if(navigation.equals("R")){
+                if(currentValue == endValue -1  ){currentValue = 0;}
+                else{currentValue ++;}
+            }
+            else if(navigation.equals("X")){break;}
+
+            for(int i = 0; i < list.size() ; i++){
+                System.out.println(i + " " + list.get(i).getUserName());
+            }
+
+            System.out.println(list.get(currentValue).getUserName());
+        }
+    }
+
+
+    public void createPost( String title , String message , String userName ){
+        Date thisDate = new Date();
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/YY");
+        String Date = date.format(thisDate);
+
+        post newpost = new post(message,Date,title,userName);
+        list.add(newpost);
+
+    }
+
+
+    public void makePost(String userName){
+        Date thisDate = new Date();
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/YY");
+        System.out.println("this is the date today " + date.format(thisDate));
+
+        String Date = date.format(thisDate);
+        String title = "";
+        String message = "";
+        int userID = 0;
+
+
+        System.out.println("please enter the title of ur post");
+        System.out.println("it has to be less than 15 letters");
+        while(true){
+
+            Scanner myObj = new Scanner(System.in);
+            String text = myObj.nextLine();
+
+            if(text.length() < 16){
+                title = text;
+                break;
+            }
+            else{
+                System.out.println("please enter the correct number of letters");
+            }
+        }
+
+
+        System.out.println("please enter you're post");
+        while(true){
+            Scanner myObj = new Scanner(System.in);
+            String text = myObj.nextLine();
+
+            if(text.length() < 250){
+                message = text;
+                break;
+            }
+            else{
+                System.out.println("please enter the correct number of letters");
+            }
+        }
+
+        post newpost = new post(message,Date,title,userName);
+        list.add(newpost);
+    }
+
+
+    public void DisplayUsersPosts(int userName){
+
+        post[] PostIDS = new post[100];
+
+        int arrayval = 0;
+        for(int i = 0; i < list.size() ; i++){
+            if(list.get(i).getUserName().equals(userName)){
+                PostIDS[arrayval] = list.get(i);
+                System.out.println("one two three" + list.get(i).getUserName());
+                arrayval++;}
+        }
+
+        String navigation;
+        int endValue = arrayval;
+        int currentValue = 0;
+
+        System.out.println(PostIDS[currentValue].getUserName());
+        while(true){
+
+            while(true){
+                System.out.println("L to go left R to go right X to quit D to delete");
+                Scanner myObj = new Scanner(System.in);
+                String text = myObj.nextLine();
+
+                if(text.equals("L") || text.equals("R") || text.equals("X") || text.equals("D")) {navigation = text; break;}
+            }
+
+            if(navigation.equals("L")){
+                if(currentValue == 0){currentValue = endValue - 1;}
+                else{currentValue --;}
+            }
+            else if(navigation.equals("R")){
+                if(currentValue == endValue -1  ){currentValue = 0;}
+                else{currentValue ++;}
+            }
+            else if(navigation.equals("D")){
+                list.remove(PostIDS[currentValue]);
+                DisplayUsersPosts(userName);
+            }
+            else if(navigation.equals("X")){break;}
+
+            for(int i = 0; i < arrayval ; i++){
+                System.out.println(i + " " + PostIDS[i].getUserName());
+            }
+
+            System.out.println(PostIDS[currentValue].getUserName() + " " + PostIDS[currentValue].getMessage());
+        }
+    }
+    //end post methods
 
     public void friendsInCommon(int userID1, int userID2) {
         Profile p1 = findNode(userID1);
