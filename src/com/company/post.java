1025e6e4f7983cbef98b8c1256966b5016f0b33a
Pronamel <114414390+Pronamel@users.noreply.github.com>
2023-03-29 15:41:20 +0100

    changes for username main identifier

--- src/com/company/post.java
+++ src/com/company/post.java
@@ -4,54 +4,54 @@ import java.util.Date;
 
 public class post
 {
-     
+
     private String message;
     private String date;
     private String title;
-    private int userID;
+    private String userName;
     private int PostID;
-    
 
-     
-    public post(String x, String z, String w, int y)
+
+
+    public post(String x, String z, String w, String y)
     {
-         
+
         message = x;
         date = z;
         title = w;
-        userID = y;
+        userName = y;
     }
 
-    
+
     public void setMessage(String y)
     {
         message = y;
     }
-    
+
     public String getMessage(){
         return message;
     }
-    
-    public int getUserID(){
-        return userID;
+
+    public String getUserName(){
+        return userName;
     }
-    
-    public void setUserID(int x){
-        userID = x;
+
+    public void setUserName(String x){
+        userName = x;
     }
-    
+
     public String getdate(){
         return date;
     }
-    
+
     public void setdate(String x){
         date = x;
     }
-    
+
     public int getPostID(){
         return PostID;
     }
-    
+
     public void setPostID(int x){
         PostID = x;
     }
